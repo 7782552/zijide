6e9a8372637c602b79d08cc49df056717686847b
7782552 <724728726@qq.com>
2026-02-08 19:18:22 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
+Python
+
 import g4f
-from g4f.Provider import Chatgpt4Online
+from g4f.Provider import Koala
 
 response = g4f.ChatCompletion.create(
     model="gpt-4",
-    provider=Chatgpt4Online,
+    provider=Koala,
     messages=[{"role": "user", "content": "你好"}],
     stream=False
 )
-print(response)
